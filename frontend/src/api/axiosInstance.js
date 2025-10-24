992078c9ab3b391c1eca4be889a0f02aa21c9b12
anasthokan <anasthokan0@gmail.com>
2025-10-24 16:51:47 +0530

    update frontend for render deployment

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // update after deploy
+   baseURL: "https://samarthkrupa-1.onrender.com/api", // update after deploy
 });
 
 export default api;
